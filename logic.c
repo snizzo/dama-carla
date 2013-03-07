021661b802a83b2fa9e7605efd62ee3315fa34cc
Claudio Desideri <happy.snizzo@gmail.com>
2013-03-07 17:18:08 +0100

    added some example functions to the game logic skeleton

--- logic.c
+++ logic.c
@@ -6,7 +6,40 @@
 
 #include "logic.h"
 
+/*
+ * This main will be ripped of in the future.
+ */
 int main()
 {
+	
+	struct board b;
+	
+	printf("INFO: setting all initial board...");
+	
+	setAllBoardEmpty(&b);
+	
+	printf("done!\n");
+	
 	return 0;
+}
+
+/*
+ * Print internal data board.
+ */
+void printBoard( struct board * b )
+{
+	// TODO: complete function that prints board
+	// mainly for debug purposes
+}
+
+/*
+ * Set all the data to empty data 'e' 
+ */
+void setAllBoardEmpty( struct board * b )
+{	
+	for (int i=0;i<8;i++){
+		for (int j=0;j<8;j++){
+			b->data[i][j] = 'e';
+		}
+	}
 }

6e56d934e141fbacb09ad9ac138a34448be23ea2
Claudio Desideri <happy.snizzo@gmail.com>
2013-03-08 23:27:15 +0100

    Added full stable network library supporting full duplex communications

--- client.c
+++ client.c
@@ -7,51 +7,40 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 
-//specific liib inclusion
+//specific lib inclusion
 #include "network_data.h"
 #include "network.h"
  
-#define MAXRCVLEN 500
-#define PORTNUM 2343
- 
 int main()
 {
-	/*
-	struct netmessage msg;
-	setNetMessage(&msg, "ciao", "pippo", "", "", "");
 	
-	char * message = (char *) &msg;
-	*/
+	struct client_network net;
 	
-	struct netmessage m;
-	setNetMessage(&m, "ciao", "pippo", "bubu", "", "");
-	char * message = (char *) &m;
+	struct netmessage message;
+	setNetMessage(&message, "ciao", "pippo", "bubu", "", "");
 	
-	char buffer[MAXRCVLEN + 1]; /* +1 so we can add null terminator */
-	int mysocket;
-	long len;
-	struct sockaddr_in dest;
- 
-	mysocket = socket(AF_INET, SOCK_STREAM, 0);
- 
-	memset(&dest, 0, sizeof(dest));                /* zero the struct */
-	dest.sin_family = AF_INET;
-	dest.sin_addr.s_addr = inet_addr("127.0.0.1"); /* set destination IP number */
-	dest.sin_port = htons(PORTNUM);                /* set destination port number */
- 
-	connect(mysocket, (struct sockaddr *)&dest, sizeof(struct sockaddr));
+	sendClientMessage(&net, &message);
 	
-	if(send(mysocket, message, 250, 0)<0){
-		printf("error sending message\n");
-	}
+	struct netmessage * incoming = readClientMessage(&net);
 	
-	len = recv(mysocket, buffer, MAXRCVLEN, 0);
- 
-	/* We have to null terminate the received data ourselves */
-	buffer[len] = '\0';
- 
-	printf("Received %s (%ld bytes).\n", buffer, len);
- 
-	close(mysocket);
-	return EXIT_SUCCESS;
+	printf("%s\n", incoming->msg1);
+	printf("%s\n", incoming->msg2);
+	printf("%s\n", incoming->msg3);
+	printf("%s\n", incoming->msg4);
+	printf("%s\n", incoming->msg5);
+	
+	sleep(2);
+	printf("done sleeping\n");
+	
+	sendClientMessage(&net, &message);
+	
+	incoming = readClientMessage(&net);
+	
+	printf("%s\n", incoming->msg1);
+	printf("%s\n", incoming->msg2);
+	printf("%s\n", incoming->msg3);
+	printf("%s\n", incoming->msg4);
+	printf("%s\n", incoming->msg5);
+	
+	return 0;
 }

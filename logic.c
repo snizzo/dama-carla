5804323a833b47aa20ba094c431d187b099ce00c
foschiani01 <foschiani01@localhost>
2013-03-09 17:59:25 +0100

    changeBoard, nextMove, move finiti; capture da completare con mangiata multipla

--- logic.c
+++ logic.c
@@ -187,20 +187,12 @@ int canBlackCapt( struct board * b, int i, int j)						//può la pedina nera man
 
 int canWkingMove( struct board * b, int i, int j )				//può la dama bianca muovere?
 {
-	if (i==0 && j==0) {
-		if (b->data[i+1][j+1]==0) {
-			return 1;
-		}
-	} else if (i==0 && j==7) {
+	if (i==0 && j==7) {
 		if (b->data[i+1][j-1]==0) {
 			return 1;
 		}
 	} else if (i==7 && j==0) {
 		if (b->data[i-1][j+1]==0) {
-			return 1;
-		}
-	} else if (i==7 && j==7) {
-		if (b->data[i-1][j-1]==0) {
 			return 1;
 		}
 	} else if (i==0) {
@@ -229,20 +221,12 @@ int canWkingMove( struct board * b, int i, int j )				//può la dama bianca muov
 
 int canWkingCapt( struct board * b, int i, int j )				//può la dama bianca mangiare?
 {
-	if (i==0 && j==0) {
-		if ((b->data[i+1][j+1]==3 || b->data[i+1][j+1]==4) && b->data[i+2][j+2]==0) {
-			return 1;
-		}
-	} else if (i==0 && j==7) {
+	if (i==0 && j==7) {
 		if ((b->data[i+1][j-1]==3 || b->data[i+1][j-1]==4) && b->data[i+2][j-2]==0) {
 			return 1;
 		}
 	} else if (i==7 && j==0) {
 		if ((b->data[i-1][j+1]==3 || b->data[i-1][j+1]==4) && b->data[i-2][j+2]==0) {
-			return 1;
-		}
-	} else if (i==7 && j==7) {
-		if ((b->data[i-1][j-1]==3 || b->data[i-1][j-1]==4) && b->data[i-2][j-2]==0) {
 			return 1;
 		}
 	} else if (i==0) {
@@ -278,20 +262,12 @@ int canWkingCapt( struct board * b, int i, int j )				//può la dama bianca mang
 
 int canBkingMove( struct board * b, int i, int j )				//può la dama nera muovere?
 {
-	if (i==0 && j==0) {
-		if (b->data[i+1][j+1]==0) {
-			return 1;
-		}
-	} else if (i==0 && j==7) {
+	if (i==0 && j==7) {
 		if (b->data[i+1][j-1]==0) {
 			return 1;
 		}
 	} else if (i==7 && j==0) {
 		if (b->data[i-1][j+1]==0) {
-			return 1;
-		}
-	} else if (i==7 && j==7) {
-		if (b->data[i-1][j-1]==0) {
 			return 1;
 		}
 	} else if (i==0) {
@@ -320,20 +296,12 @@ int canBkingMove( struct board * b, int i, int j )				//può la dama nera muover
 
 int canBkingCapt( struct board * b, int i, int j )				//può la dama nera mangiare?
 {
-	if (i==0 && j==0) {
-		if ((b->data[i+1][j+1]==1 || b->data[i+1][j+1]==2) && b->data[i+2][j+2]==0) {
-			return 1;
-		}
-	} else if (i==0 && j==7) {
+	if (i==0 && j==7) {
 		if ((b->data[i+1][j-1]==1 || b->data[i+1][j-1]==2) && b->data[i+2][j-2]==0) {
 			return 1;
 		}
 	} else if (i==7 && j==0) {
 		if ((b->data[i-1][j+1]==1 || b->data[i-1][j+1]==2) && b->data[i-2][j+2]==0) {
-			return 1;
-		}
-	} else if (i==7 && j==7) {
-		if ((b->data[i-1][j-1]==1 || b->data[i-1][j-1]==2) && b->data[i-2][j-2]==0) {
 			return 1;
 		}
 	} else if (i==0) {
@@ -370,11 +338,150 @@ int canBkingCapt( struct board * b, int i, int j )				//può la dama nera mangia
 
 /*
  * i,j=coordinate pedina/dama da spostare; k,l=coordinate "arrivo"; m= 1 bianco 2 nero
+ * prima di nextMove bisogna far chiamare al programma canMove, se canMove dà -1 la partita è finita e nextMove non parte
+ * promemoria: capture controlla se una delle pedine che possono mangiare è quella selezionata dal giocatore. se lo è
+ * e il giocatore ha selezionato la giusta casella per mangiare, modifica la board e il turno finisce, se una delle due
+ * condizioni non è soddisfatta dice "devi mangiare per forza, rifai la mossa"; move funzionerà in modo simile
  */
-int nextMove( struct board * b, int i, int j, int k, int l, int m )		//dà per scontato che la casella di partenza e quella di
+void nextMove( struct board * b, int i, int j, int k, int l, int m )	//dà per scontato che la casella di partenza e quella di
 {																			//arrivo appartengano alla board
-	return 1;
+	if (m==1) {																//gioca il bianco
+		if ((b->data[i][j]==1 || b->data[i][j]==2) && b->data[k][l]==0) {
+			for (int d=0;d<8;d++){
+				for (int e=0;e<8;e++){
+					if (canWhiteCapt(b, d, e)==1 || canWkingCapt(b, d, e)==1) {
+						capture(b, i, j, k, l);
+					} else {
+						move(b, i, j, k, l);
+					}
+				}
+			}
+		} else {
+			printf ("quella non è una tua pedina/dama oppure la casella di destinazione non è libera\n");
+		}
+	} else {																//gioca il nero
+		if ((b->data[i][j]==3 || b->data[i][j]==4) && b->data[k][l]==0) {
+			for (int d=0;d<8;d++){
+				for (int e=0;e<8;e++){
+					if (canBlackCapt(b, d, e)==1 || canWkingCapt(b, d, e)==1) {
+						capture(b, i, j, k, l);
+					} else {
+						move(b, i, j, k, l);
+					}
+				}
+			}
+		} else {
+			printf ("quella non è una tua pedina/dama oppure la casella di destinazione non è libera\n");
+		}
+	}
 }
+
+int move( struct board *b, int i, int j, int k, int l)
+{
+	if (b->data[i][j]==1) {
+		if (k==i-1 && (l==j+1 || l==j-1)) {
+			changeBoard(b, i, j, 0);
+			changeBoard(b, k, l, 1);
+			return 1;
+		} else {
+			printf ("mossa non consentita\n");
+			return -1;
+		}
+	} else if (b->data[i][j]==2) {
+		if ((k==i+1 || k==i-1) && (l==j+1 || l==j-1)) {
+			changeBoard(b, i, j, 0);
+		    changeBoard(b, k, l, 2);
+		    return 1;
+		} else {
+			printf ("mossa non consentita\n");
+			return -1;
+		}
+	} else if (b->data[i][j]==3) {
+		if (k==i+1 && (l==j+1 || l==j-1)) {
+			changeBoard(b, i, j, 0);
+			changeBoard(b, k, l, 3);
+			return 1;
+		} else {
+			printf ("mossa non consentita\n");
+			return -1;
+		}
+	} else if (b->data[i][j]==4) {
+		if ((k==i+1 || k==i-1) && (l==j+1 || l==j-1)) {
+			changeBoard(b, i, j, 0);
+		    changeBoard(b, k, l, 4);
+		    return 1;
+		} else {
+			printf ("mossa non consentita\n");
+			return -1;
+		}
+	}
+}
+
+/*
+ * se il capture va a buon fine, si dovrebbe verificare se la pedina che ha appena mangiato è ancora in grado di mangiare,
+ * in quel caso bisogna farglielo fare però il giocatore dovrebbe decidere quale mangiare se può mangiarne più di una
+ */
+int capture( struct board *b, int i, int j, int k, int l)
+{
+	if (b->data[i][j]==1 && i==k+2 && (l==j-2 || l==j+2)) {
+		if (b->data[(i+k)/2][(j+l)/2]==3 || b->data[(i+k)/2][(j+l)/2]==4) {
+			changeBoard(b, i, j, 0);
+			changeBoard(b, (i+k)/2, (j+l)/2, 0);
+			changeBoard(b, k, l, 1);
+			return 1;
+		} else {
+			printf ("mossa non consentita\n");
+			return -1;
+		}
+	} else if (b->data[i][j]==2 && (i==k+2 || i==k-2) && (j==l-2 || j==l+2)) {
+		if (b->data[(i+k)/2][(j+l)/2]==3 || b->data[(i+k)/2][(j+l)/2]==4) {
+			changeBoard(b, i, j, 0);
+			changeBoard(b, (i+k)/2, (j+l)/2, 0);
+			changeBoard(b, k, l, 2);
+			return 1;
+		} else {
+			printf ("mossa non consentita\n");
+			return -1;
+		}
+	} else if (b->data[i][j]==3 && i==k-2 && (l==j-2 || l==j+2)) {
+		if (b->data[(i+k)/2][(j+l)/2]==3 || b->data[(i+k)/2][(j+l)/2]==4) {
+			changeBoard(b, i, j, 0);
+			changeBoard(b, (i+k)/2, (j+l)/2, 0);
+			changeBoard(b, k, l, 3);
+			return 1;
+		} else {
+			printf ("mossa non consentita\n");
+			return -1;
+		}
+	} else if (b->data[i][j]==2 && (i==k+2 || i==k-2) && (j==l-2 || j==l+2)) {
+		if (b->data[(i+k)/2][(j+l)/2]==3 || b->data[(i+k)/2][(j+l)/2]==4) {
+			changeBoard(b, i, j, 0);
+			changeBoard(b, (i+k)/2, (j+l)/2, 0);
+			changeBoard(b, k, l, 4);
+			return 1;
+		} else {
+			printf ("mossa non consentita\n");
+			return -1;
+		}
+	}
+}
+		
+
+
+/*
+ * i,j=coordinate da modificare. c=tipo di pedina
+ */
+void changeBoard( struct board * b, int i, int j,int c)
+{
+	if (i==0 && c==1) {
+		b->data[i][j]=2;
+	} else if (i==7 && c==3) {
+		b->data[i][j]=4;
+	} else {
+		b->data[i][j]=c;
+	}
+}
+
 
 /*
  * Print internal data board.

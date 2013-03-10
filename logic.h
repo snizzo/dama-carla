c438202607e4346acb98fdf290ce99a933e1a93a
foschiani01 <foschiani01@localhost>
2013-03-10 11:48:41 +0100

    logic.c finito

--- logic.h
+++ logic.h
@@ -18,7 +18,7 @@ struct board{
 	int data[8][8];
 };
 
-void nextMove( struct board * b, int i, int j, int k, int l, int m );
+int nextMove( struct board * b, int i, int j, int k, int l, int m );
 void prepareBoard( struct board * b );
 void printBoard( struct board * b );
 void setAllBoardEmpty( struct board * b );

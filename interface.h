a34adb5546e8e2dd77fe15ba35561c08fb0589bf
Claudio Desideri <happy.snizzo@gmail.com>
2013-03-19 20:24:01 +0100

    Added some more complex interface to client. Added ncurses form library.

---
+++ interface.h
@@ -0,0 +1,11 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+//ncurses lib inclusion
+#include <ncurses.h>
+#include <form.h>
+
+void setInterface(void);
+int showLoginForm(void);
+void unsetInterface(void);
+
+#endif

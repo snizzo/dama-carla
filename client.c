f44c11968b80f67f4527c46840344c679dc5e06d
Claudio Desideri <happy.snizzo@gmail.com>
2013-03-24 18:29:34 +0100

    Added easy signup process for client.

--- client.c
+++ client.c
@@ -11,27 +11,32 @@
 #include "interface.h"
 #include "client.h"
 
-int main()
+void evaluateParams(int argc, char * argv[]);
+
+int main(int argc, char * argv[])
 {
+	//evaluating additional parameters
+	evaluateParams(argc,argv);
 	
+	//initializing client network
 	struct client_network net;
 	
+	//creating the user structure that will take record of the user info and current state
 	struct clientuser * me = malloc(sizeof(struct clientuser));
 	me->logged = 0;
 	
 	setInterface(); //setting our program to use ncurses
 	
+	//anonymous play not supported - force user to log in
 	while(!me->logged){
 		struct logininfo * logindata = NULL;
 		
-		logindata = showLoginForm(); //show login form
+		logindata = showLoginForm(); //show login form, login data is allocated, needs to be free
 		
 		singleWindowMessage("logging...");
 		
 		
-		fullClientCommand(&net, "login", logindata->username, logindata->password, "", ""); //TODO: fixme
-		
-		//fullClientCommand(&net, "login", logindata->username, logindata->password, "", ""); //TODO: fixme
+		fullClientCommand(&net, "login", logindata->username, logindata->password, "", "");
 		
 		struct netmessage * incoming = readClientMessage(&net);
 		
@@ -47,16 +52,19 @@ int main()
 			me->wins = 0;
 			me->losses = 0;
 			
-			sleep(2); //let the user read this
+			sleep(2);
 		} else if(areEqual(incoming->msg1, "wrongpassword")) {
 			singleWindowMessage("Wrong Password!");
-			sleep(2); //let the user read this
+			sleep(2);
+			
 		} else if(areEqual(incoming->msg1, "usernotfound")) {
 			singleWindowMessage("User Not Found!");
-			sleep(2); //let the user read this
+			sleep(2);
+			
 		} else {
 			singleWindowMessage("Unknown Error!");
-			sleep(2); //let the user read this
+			sleep(2);
+			
 		}
 		
 		free(logindata->username);
@@ -67,119 +75,47 @@ int main()
 	
 	unsetInterface(); //unset ncurses
 	
-/*	
-	struct board b;
-	
-	
-	struct client_network net;
-	
-	//login
-	struct netmessage message;
-	setNetMessage(&message, "login", "snizzo", "mypwd", "", "");
-	
-	sendClientMessage(&net, &message);
-	
-	struct netmessage * incoming = readClientMessage(&net);
-	
-	printf("%s\n", incoming->msg1);
-	printf("%s\n", incoming->msg2);
-	printf("%s\n", incoming->msg3);
-	printf("%s\n", incoming->msg4);
-	printf("%s\n", incoming->msg5);
-	
-	
-	sleep(2);
-	
-	char * loginkey = copystring(incoming->msg2);
-	
-	//authping, incoming->msg2 contains the authentication key
-	setNetMessage(&message, "authping", "", "", "", loginkey);
-	
-	sendClientMessage(&net, &message);
-	
-	incoming = readClientMessage(&net);
-	
-	printf("%s\n", incoming->msg1);
-	printf("%s\n", incoming->msg2);
-	printf("%s\n", incoming->msg3);
-	printf("%s\n", incoming->msg4);
-	printf("%s\n", incoming->msg5);
-	
-
-	initscr();
-	mvprintw(0, 0,"Nord-ovest"); 
-	mvprintw(LINES-1, 0,"Sud-ovest");
-	mvprintw(0, COLS-8,"Nord-est"); 
-	mvprintw(LINES-1, COLS-7,"Sud-est");
-	
-	start_color();
-	init_pair(1, COLOR_WHITE, COLOR_WHITE);
-	init_pair(2, COLOR_WHITE, COLOR_BLACK);
-	init_pair(3, COLOR_MAGENTA, COLOR_BLACK);
-	init_pair(4, COLOR_BLACK, COLOR_BLACK);
-	
-	b.data[0][7] = 3;
-	b.data[0][5] = 3;
-	b.data[0][3] = 3;
-	b.data[0][1] = 0;
-	
-	b.data[1][6] = 2;
-	b.data[1][4] = 4;
-	b.data[1][2] = 4;
-	b.data[1][0] = 0;
-	
-	b.data[2][7] = 0;
-	b.data[2][5] = 3;
-	b.data[2][3] = 3;
-	b.data[2][1] = 0;
-	
-	b.data[7][6] = 0;
-	b.data[7][4] = 1;
-	b.data[7][2] = 0;
-	b.data[7][0] = 1;
-	
-	b.data[5][6] = 1;
-	b.data[5][4] = 2;
-	b.data[5][2] = 1;
-	b.data[5][0] = 0;
-	
-	b.data[6][7] = 0;
-	b.data[6][5] = 1;
-	b.data[6][3] = 1;
-	b.data[6][1] = 0;
-	
-	for (int i=0;i<8;i++){
-			for (int j=0;j<8;j++){
-				if (even(i+j)) {
-					attron(COLOR_PAIR(1));
-					mvprintw(i, j, "e");
-				} else if (b.data[i][j]==1) {
-					attron(COLOR_PAIR(2));
-					mvprintw(i, j, "@");
-				} else if (b.data[i][j]==2) {
-					attron(COLOR_PAIR(2));
-					mvprintw(i, j, "W");
-				} else if (b.data[i][j]==3) {
-					attron(COLOR_PAIR(3));
-					mvprintw(i, j, "@");
-				} else if (b.data[i][j]==4) {
-					attron(COLOR_PAIR(3));
-					mvprintw(i, j, "W");
-				} else {
-					attron(COLOR_PAIR(4));
-					mvprintw(i, j, "e");
-				}
-			}
-		}
-				
-
-	refresh();  
-	getch();  
-	endwin(); 
-*/	
 	return 0;
 }
 
+/*
+ * Evaluate additional lauch parameters
+ */
+void evaluateParams(int argc, char * argv[])
+{
+	if ((argc>1) && (areEqual("--help", argv[1]))){
+		printf("Dama Carla - Online Dama Client\n");
+		printf("Port   : 2343\n");
+		printf("Author : Claudio Desideri <happy.snizzo@gmail.com>\n");
+		printf("Website: http://dama-carla.googlecode.com\n");
+		printf("\n");
+		printf("\n");
+		printf("Usage:\n");
+		printf("\n");
+		printf("register <nickname> <password> :\n");
+		printf("Register a new account on server with given <nickname> and <password>\n");
+		exit(0);
+	} else if ((argc>=4) && (areEqual("register", argv[1]))){
+		//initializing client network
+		struct client_network net;
+		fullClientCommand(&net, "register", argv[2], argv[3], "", "");
+		
+		struct netmessage * incoming = readClientMessage(&net);
+		if((areEqual(incoming->msg1,"done"))){
+			printf("done\n");
+			exit(0);
+		} else if ((areEqual(incoming->msg1,"alreadythere"))) {
+			printf("user already present in database\n");
+			exit(0);
+		} else {
+			printf("unknown error :(\n");
+			printf("feel free to report a bug to http://dama-carla.googlecode.com\n");
+			exit(0);
+		}
+	}
+	
+	
+}
 
 //various utils
 int even(int n) {

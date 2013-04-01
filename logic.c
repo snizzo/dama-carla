07ac1efce6b853dfa07d6a606dd6cf1bd0098cf1
foschiani01 <foschiani01@localhost>
2013-04-01 22:49:49 +0200

    fix

--- logic.c
+++ logic.c
@@ -16,11 +16,7 @@ int main()
 	
 	struct board b;
 	
-	printf("INFO: setting all initial board...");
-	
 	setAllBoardEmpty(&b);
-	
-	printf("done!\n");
 	
 	prepareBoard(&b);
 	
@@ -29,8 +25,6 @@ int main()
 	nextMove(&b, 3, 2, 1, 4, 1);
 
 	printBoard(&b);
-	
-	mirrorBoard(&b);
 	
 	return 0;
 }
@@ -77,14 +71,15 @@ int canMove ( struct board * b, int m)
 	int c = 0;													//orribile contatore che conta se c'è almeno una mossa che il
 	if (m==1) {													//giocatore può fare
 		c=0;
-		for (int i=0;i<8;i++){
-			for (int j=0;j<8;j++){
-				if (b->data[i][j]==1) {
-					if ((canWhiteMove(b, i, j)==1) || (canWhiteCapt(b, i, j)==1)) {
+		for (int d=0;d<8;d++){
+			for (int e=0;e<8;e++){
+				if (b->data[d][e]==1) {
+					if ((canWhiteMove(b, d, e)==1) || ((canWhiteCapt(b, d, e, d-1, e-1)==1) || (canWhiteCapt(b, d, e, d-1, e+1)==1))) {
 						c++;
 					}
-				} else if (b->data[i][j]==2) {
-					if ((canWkingMove(b, i ,j)==1) || (canWkingCapt(b, i ,j)==1)) {
+				} else if (b->data[d][e]==2) {
+					if ((canWkingMove(b, d, e)==1) || ((canWkingCapt(b, d, e, d-1, e-1)==1) || (canWkingCapt(b, d, e, d-1, e+1)==1) ||
+													   (canWkingCapt(b, d, e, d+1, e-1)==1) || (canWkingCapt(b, d, e, d+1, e+1)==1))) {
 						c++;
 					}
 				}
@@ -92,14 +87,15 @@ int canMove ( struct board * b, int m)
 		}
 	} else if (m==2) {
 		c=0;
-		for (int i=0;i<8;i++){
-			for (int j=0;j<8;j++){
-				if (b->data[i][j]==3) {
-					if ((canBlackMove(b, i, j)==1) || (canBlackCapt(b, i, j)==1)) {
+		for (int d=0;d<8;d++){
+			for (int e=0;e<8;e++){
+				if (b->data[d][e]==3) {
+					if ((canBlackMove(b, d, e)==1) || ((canBlackCapt(b, d, e, d+1, e-1)==1) || (canBlackCapt(b, d, e, d+1, e+1)==1))) {
 						c++;
 					}
-				} else if (b->data[i][j]==4) {
-					if ((canBkingMove(b, i ,j)==1) || (canBkingCapt(b, i ,j)==1)) {
+				} else if (b->data[d][e]==4) {
+					if ((canBkingMove(b, d ,e)==1) || ((canBkingCapt(b, d, e, d+1, e-1)==1) || (canBkingCapt(b, d, e, d+1, e+1)==1) ||
+													   (canBkingCapt(b, d, e, d-1, e-1)==1) || (canBkingCapt(b, d, e, d-1, e+1)==1))) {
 						c++;
 					}
 				}
@@ -133,9 +129,11 @@ int canWhiteMove( struct board * b, int i, int j )				//può la pedina bianca mu
 	return -1;
 }
 
-int canWhiteCapt( struct board * b, int i, int j)						//può la pedina bianca mangiare?
+int canWhiteCapt( struct board * b, int i, int j, int k, int l)						//può la pedina bianca mangiare?
 {
-	if (j==0) {
+	if (notCapturable(k, l)==1) {
+		return -1;
+	} else if (j==0) {
 		if ((b->data[i-1][j+1]==3 || b->data[i-1][j+1]==4) && b->data[i-2][j+2]==0) {
 			return 1;
 		}
@@ -169,9 +167,11 @@ int canBlackMove( struct board * b, int i, int j )				//può la pedina nera muov
 	return -1;
 }
 
-int canBlackCapt( struct board * b, int i, int j)						//può la pedina nera mangiare?
+int canBlackCapt( struct board * b, int i, int j, int k, int l)						//può la pedina nera mangiare?
 {
-	if (j==0) {
+	if (notCapturable(k, l)==1) {
+		return -1;
+	} else if (j==0) {
 		if ((b->data[i+1][j+1]==1 || b->data[i+1][j+1]==2) && b->data[i+2][j+2]==0) {
 			return 1;
 		}
@@ -221,9 +221,11 @@ int canWkingMove( struct board * b, int i, int j )				//può la dama bianca muov
 	return -1;
 }
 
-int canWkingCapt( struct board * b, int i, int j )				//può la dama bianca mangiare?
+int canWkingCapt( struct board * b, int i, int j, int k, int l)				//può la dama bianca mangiare?
 {
-	if (i==0 && j==7) {
+	if (notCapturable(k, l)==1) {
+		return -1;
+	} else if (i==0 && j==7) {
 		if ((b->data[i+1][j-1]==3 || b->data[i+1][j-1]==4) && b->data[i+2][j-2]==0) {
 			return 1;
 		}
@@ -296,9 +298,11 @@ int canBkingMove( struct board * b, int i, int j )				//può la dama nera muover
 	return -1;
 }
 
-int canBkingCapt( struct board * b, int i, int j )				//può la dama nera mangiare?
+int canBkingCapt( struct board * b, int i, int j, int k, int l)				//può la dama nera mangiare?
 {
-	if (i==0 && j==7) {
+	if (notCapturable(k, l)==1) {
+		return -1;
+	} else if (i==0 && j==7) {
 		if ((b->data[i+1][j-1]==1 || b->data[i+1][j-1]==2) && b->data[i+2][j-2]==0) {
 			return 1;
 		}
@@ -398,11 +402,12 @@ int nextMove( struct board * b, struct moveinfo * move, int m )		//dà per scont
 			for (int d=0;d<8;d++){
 				for (int e=0;e<8;e++){
 					if (b->data[d][e]==1) {
-						if (canWhiteCapt(b, d, e)==1) {
+						if ((canWhiteCapt(b, d, e, d-1, e-1)==1) || (canWhiteCapt(b, d, e, d-1, e+1)==1)) {
 							c++;
 						}
 					} else if (b->data[d][e]==2) {
-						if (canWkingCapt(b, d, e)==1) {
+						if ((canWkingCapt(b, d, e, d-1, e-1)==1) || (canWkingCapt(b, d, e, d-1, e+1)==1) ||
+							(canWkingCapt(b, d, e, d+1, e-1)==1) || (canWkingCapt(b, d, e, d+1, e+1)==1)) {
 							c++;
 						}
 					}
@@ -428,11 +433,12 @@ int nextMove( struct board * b, struct moveinfo * move, int m )		//dà per scont
 			for (int d=0;d<8;d++){
 				for (int e=0;e<8;e++){
 					if (b->data[d][e]==3) {
-						if (canBlackCapt(b, d, e)==1) {
+						if ((canBlackCapt(b, d, e, d+1, e-1)==1) || (canBlackCapt(b, d, e, d+1, e+1)==1)) {
 							c++;
 						}
 					} else if (b->data[d][e]==4){
-						if(canBkingCapt(b, d, e)==1){
+						if ((canBkingCapt(b, d, e, d+1, e-1)==1) || (canBkingCapt(b, d, e, d+1, e+1)==1) ||
+							(canBkingCapt(b, d, e, d-1, e-1)==1) || (canBkingCapt(b, d, e, d-1, e+1)==1)) {
 							c++;
 						}
 					}
@@ -558,6 +564,14 @@ void changeBoard( struct board * b, int i, int j,int c)
 		b->data[i][j]=4;
 	} else {
 		b->data[i][j]=c;
+	}
+}
+
+int notCapturable (int k, int l) {
+	if (k<1 || l<1 || k>6 || l>6) {
+		return 1;
+	} else {
+		return 0;
 	}
 }
 
